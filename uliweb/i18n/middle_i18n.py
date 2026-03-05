19d588d3dfa417cdba6d41b25d34db62fed7f690
Chunlin Zhang <zhangclb@lenovo.com>
2026-03-05 18:59:48 +0800

    继续修改 contrib 里的 asgi

--- uliweb/i18n/middle_i18n.py
+++ uliweb/i18n/middle_i18n.py
@@ -2,6 +2,8 @@ import re
 
 from uliweb.i18n import set_language, format_locale
 from uliweb import Middleware
+from logging import getLogger
+from uliweb.utils.common import request_url
 
 accept_language_re = re.compile(r'''
         ([A-Za-z]{1,8}(?:-[A-Za-z]{1,8})*|\*)   # "en", "en-au", "x-y-z", "*"
@@ -12,26 +14,40 @@ accept_language_re = re.compile(r'''
 def get_language_from_request(request, settings):
     #check query_string, and the key will be defined in settings.ini
     #now only support GET method
+    debug = '__debug__' in request.GET
+    log = getLogger(__name__)
     url_lang_key = settings.get_var('I18N/URL_LANG_KEY')
     if url_lang_key:
         lang = request.GET.get(url_lang_key)
         if lang:
+            if debug:
+                log.info('Detect from URL=%s, lang_key=%s, lang=%s' %
+                         (request_url(), url_lang_key, lang))
             return lang
 
     #check session
     if hasattr(request, 'session'):
         lang = request.session.get('uliweb_language')
         if lang:
+            if debug:
+                log.info('Detect from session=%s, lang=%s' %
+                         ('uliweb_language', lang))
             return lang
 
     #check cookie
     lang = request.cookies.get(settings.I18N.LANGUAGE_COOKIE_NAME)
     if lang:
+        if debug:
+            log.info('Detect from cookie=%s, lang=%s' %
+                     (settings.I18N.LANGUAGE_COOKIE_NAME, lang))
         return lang
 
     #check browser HTTP_ACCEPT_LANGUAGE head
-    accept = request.environ.get('HTTP_ACCEPT_LANGUAGE', None)
+    # For ASGI compatibility, use request.headers instead of request.environ
+    accept = request.headers.get('accept-language', None)
     if not accept:
+        if debug:
+            log.info('Detect from settings of LANGUAGE_CODE=%s' % lang)
         return settings.I18N.get('LANGUAGE_CODE')
     languages = settings.I18N.get('SUPPORT_LANGUAGES', [])
     for accept_lang, unused in parse_accept_lang_header(accept):
@@ -41,12 +57,18 @@ def get_language_from_request(request, settings):
         normalized = format_locale(accept_lang)
         if not normalized:
             continue
-        
+
         if normalized in languages:
+            if debug:
+                log.info('Detect from HTTP Header=%s, lang=%s' %
+                         ('accept-language', normalized))
             return normalized
 
     #return default lanaguage
-    return settings.I18N.get('LANGUAGE_CODE')
+    lang = settings.I18N.get('LANGUAGE_CODE')
+    if debug:
+        log.info('Detect from settings of LANGUAGE_CODE=%s' % lang)
+    return lang
 
 def parse_accept_lang_header(lang_string):
     """
@@ -60,16 +82,23 @@ def parse_accept_lang_header(lang_string):
     if pieces[-1]:
         return []
     for i in range(0, len(pieces) - 1, 3):
-        first, lang, priority = pieces[i : i + 3]
+        first, lang, priority = pieces[i: i + 3]
         if first:
             return []
         priority = priority and float(priority) or 1.0
         result.append((lang, priority))
-    result.sort(lambda x, y: -cmp(x[1], y[1]))
+    result.sort(key=lambda x: -x[1])
     return result
 
 class I18nMiddle(Middleware):
-    def process_request(self, request):
+    async def dispatch(self, request, call_next):
+        # 请求预处理
         lang = get_language_from_request(request, self.settings)
         if lang:
             set_language(lang)
+
+        # 调用下一个中间件或视图函数
+        response = await call_next(request)
+
+        # I18n中间件没有响应后处理逻辑
+        return response
