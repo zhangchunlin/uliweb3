b0468c62cedee2383a6cb24a1ddbc01b10ab7819
Chunlin Zhang <zhangclb@lenovo.com>
2026-03-05 18:54:55 +0800

    继续修改contrib.orm的asgi

--- uliweb/contrib/orm/middle_notfound.py
+++ uliweb/contrib/orm/middle_notfound.py
@@ -1,13 +1,28 @@
 from uliweb import Middleware, error
 from uliweb.orm import NotFound
 
+
 class ORMNotfoundMiddle(Middleware):
+    """
+    异步 ORM NotFound 处理中间件 - 使用 ASGI dispatch 接口
+
+    将原来的 process_exception 方法整合到异步 dispatch 方法中。
+    """
     ORDER = 110
-    
+
     def __init__(self, application, settings):
         pass
-        
-    def process_exception(self, request, exception):
-        if isinstance(exception, NotFound):
-            error("%s(%s) can't be found" % (exception.model.__name__, exception.id))
-        
+
+    async def dispatch(self, request, call_next):
+        """
+        异步调度方法 - 处理请求和异常
+        """
+        try:
+            # 调用下一个中间件或视图函数
+            response = await call_next(request)
+            return response
+        except Exception as e:
+            # 异常处理 - 检查是否为 NotFound 异常
+            if isinstance(e, NotFound):
+                error("%s(%s) can't be found" % (e.model.__name__, e.id))
+            raise
--- uliweb/contrib/orm/middle_orm_reset.py
+++ uliweb/contrib/orm/middle_orm_reset.py
@@ -1,9 +1,23 @@
 from uliweb import Middleware
 from uliweb.orm import set_dispatch_send, set_echo
 
+
 class ORMResetMiddle(Middleware):
+    """
+    异步 ORM 重置中间件 - 使用 ASGI dispatch 接口
+
+    将原来的 process_request 方法整合到异步 dispatch 方法中。
+    """
     ORDER = 70
-    
-    def process_request(self, request):
+
+    async def dispatch(self, request, call_next):
+        """
+        异步调度方法 - 处理请求预处理
+        """
+        # 请求预处理 - 重置 ORM 状态
         set_echo(False)
         set_dispatch_send(True)
+
+        # 调用下一个中间件或视图函数
+        response = await call_next(request)
+        return response
