8389b6cc9f9cbc3b6492daf5480eba84bac60dfe
Chunlin Zhang <zhangclb@lenovo.com>
2026-03-07 17:01:03 +0800

    修复 sendmail 里的问题

--- uliweb/mail/backends/sendmail.py
+++ uliweb/mail/backends/sendmail.py
@@ -1,5 +1,5 @@
-import smtplib
 from .smtp import MailConnection as SmtpMailConnection
+
 
 class MailConnection(SmtpMailConnection):
     def login(self):
@@ -7,12 +7,12 @@ class MailConnection(SmtpMailConnection):
 
     def get_connection(self):
         pass
-            
+
     def send_mail(self, from_, to_, message):
         from subprocess import Popen, PIPE
-        
-        p = Popen([self.sendmail_location, "-t"], stdin=PIPE)
+
+        p = Popen([self.mail_obj.sendmail_location, "-t"], stdin=PIPE)
         p.communicate(str(message))
-    
+
     def close(self):
         pass
