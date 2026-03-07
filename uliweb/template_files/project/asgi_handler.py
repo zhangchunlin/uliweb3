52dad6af1a8c0eef72b10f48b57309bdec3901fe
Chunlin Zhang <zhangclb@lenovo.com>
2026-03-07 23:00:08 +0800

    template 里 asgi_handler 改成 asgi 方式

--- uliweb/template_files/project/asgi_handler.py
+++ uliweb/template_files/project/asgi_handler.py
@@ -16,6 +16,7 @@ path = os.path.dirname(os.path.abspath(__file__))
 if path not in sys.path:
     sys.path.insert(0, path)
 
-from uliweb.manage import make_simple_application
+from uliweb.core.starlette import ASGIApplication
 
-application = make_simple_application(project_dir=path)
+# 使用纯 ASGI 处理程序
+application = ASGIApplication(project_dir=path)
