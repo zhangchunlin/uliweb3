dc74453dc1bdb38c87b227852e72e81a46555e63
Chunlin Zhang <zhangclb@lenovo.com>
2026-03-07 16:04:51 +0800

    改掉python3不支持的cmp

--- uliweb/core/dispatch.py
+++ uliweb/core/dispatch.py
@@ -133,10 +133,7 @@ def get(sender, topic, *args, **kwargs):
     if not topic in _receivers:
         return
     items = _receivers[topic]
-    def _cmp(x, y):
-        return cmp(x[0], y[0])
-
-    items.sort(_cmp)
+    items.sort(key=lambda x: x[0])
     for i in range(len(items)):
         nice, f = items[i]
         _f = f['func']
